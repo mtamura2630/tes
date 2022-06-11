654eebed1793a143762e5f809fad9d6223363d1e
mtamura2630 <mtamura.2630@gmail.com>
2022-06-11 14:58:19 +0000

    commit

--- test.py
+++ test.py
@@ -1,5 +1,13 @@
 import numpy as np
+import sklearn as sk
+import requests as req
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import scipy.stats as stats
+import statsmodels.api as sm
 
 a = np.array([[1,2,3],[4,5,6],[7,8,9]])
 
 print(a)
+
